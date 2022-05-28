251c240304954d450d4211754498c02a15890ea8
yanto <ekocahyanto007@gmail.com>
2022-05-28 18:40:46 +0700

    challange 6

--- index.js
+++ index.js
@@ -23,19 +23,33 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    let data = dataYangAkanDilooping[index].namaDepan + " " + dataYangAkanDilooping[index].namaBelakang;
+    hasilLooping.push(data);
+  }
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
   let jumlahPria = null;
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    if (dataYangAkanDilooping[index].jenisKelamin === "M") {
+      jumlahPria += 1;
+    }
+  }
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
   let jumlahWanita = null;
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    if (dataYangAkanDilooping[index].jenisKelamin === "F") {
+      jumlahWanita += 1;
+    }
+  }
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -49,6 +63,13 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  } else if (jumlahWanita > jumlahPria) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  } else {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
@@ -65,6 +86,7 @@ function main(data) {
   console.log(hasil.hasilLooping);
   console.log(hasil.jumlahPria);
   console.log(hasil.jumlahWanita);
+  console.log(hasil.komentar);
 
   return hasil;
 }
